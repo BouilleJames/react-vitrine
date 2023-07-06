b05c5ae6f2ebbe4c97f8f2e22d46ce5673d9a43b
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-07-06 23:52:30 +0200

    add site

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,28 +1,38 @@
 export const projectsData = [
   {
     id: 1,
+    title: "James Bouillé",
+    date: "Juin 2023",
+    languages: ["Bootstrap"],
+    infos:
+      "J'ai fait mon CV avec Bootstrap pour essayer cette Techno et voir ces qualités et ses défauts.",
+    img: "./assets/img/cvJames.png",
+    link: "https://cv-james-bouille.netlify.app/",
+  },
+  {
+    id: 2,
     title: "Flags of the world",
     date: "Juin 2023",
     languages: ["React", "Sass"],
     infos:
       "J'ai reproduit ce tuto de FromScratch pour voir comment il fonctionnait.",
     img: "./assets/img/projet-2.jpg",
-    link: "http://www.google.com",
-  },
-  {
-    id: 2,
-    title: "Lyon Béton",
-    date: "Mars 2020",
-    languages: ["Symfony", "Vue"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-1.jpg",
-    link: "http://www.google.com",
+    link: "https://resilient-meringue-414ca0.netlify.app/",
   },
   {
     id: 3,
-    title: "Everpost",
-    date: "Avril 2020",
+    title: "portfolio",
+    date: "juin 2023",
+    languages: ["React", "Sass"],
+    infos:
+      "J'aime bien ce portfolio, je l'ai fait avec React et Sass, je l'ai trouvé sur FromScratch et je l'ai reproduit.Je compte bien le modifier pour le rendre plus personnel.",
+    img: "./assets/img/projet-3.png",
+    link: "http://www.google.com",
+  },
+  {
+    id: 4,
+    title: "What Watch",
+    date: "Juillet 2023",
     languages: ["Wordpress", "Php", "React"],
     infos:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
